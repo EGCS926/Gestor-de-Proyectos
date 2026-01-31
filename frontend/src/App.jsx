f6b70d2a0ffda627738b59fcefd79f47e45ab5df
Elias Gamaliel Castro Sandoval <castrogamaliel60@gmail.com>
2026-01-30 21:05:11 -0600

    Modificacion de App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList";
+import { useState } from "react";
+import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
 
 export default function App() {
+  const [refresh, setRefresh] = useState(0);
+
+  const reloadProjects = () => setRefresh(prev => prev + 1);
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Gestor de Tareas</h1>
-      <ProjectList />
-      <TaskForm />
-      <TaskList />
+    <div>
+      <h1>Gestor de Proyectos</h1>
+      <ProjectForm onProjectCreated={reloadProjects} />
+      <ProjectList refresh={refresh} />
     </div>
   );
 }

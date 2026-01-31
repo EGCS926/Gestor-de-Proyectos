9f6d687f31594658ace0c52c1f216f61cf443ecd
Elias Gamaliel Castro Sandoval <castrogamaliel60@gmail.com>
2026-01-31 16:27:52 -0600

    Modificacion de Project.controller.js

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -1,11 +1,3 @@
-import Project from "../models/Project.js";
-
 export const getProjects = async (req, res) => {
-  const projects = await Project.find();
-  res.json(projects);
-};
-
-export const createProject = async (req, res) => {
-  const project = await Project.create(req.body);
-  res.status(201).json(project);
+  res.json({ message: "Lista de proyectos" });
 };

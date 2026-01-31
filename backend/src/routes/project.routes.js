b32c1b11225990dcef2ea3be6873b3e5f4219692
Elias Gamaliel Castro Sandoval <castrogamaliel60@gmail.com>
2026-01-31 16:23:35 -0600

    modificacion de project.routers.js 2

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -4,11 +4,5 @@ import { getProjects } from "../controllers/project.controller.js";
 const router = Router();
 
 router.get("/", getProjects);
-router.post("/", createProject);
 
 export default router;
-
-export const getProjects = async (req, res) => {
-  const projects = await Project.find();
-  res.json(projects);
-};

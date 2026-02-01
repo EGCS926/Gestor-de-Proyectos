e3fb31007bbaf0861d8b625fb6879bdf0101998b
Elias Gamaliel Castro Sandoval <castrogamaliel60@gmail.com>
2026-01-31 18:06:32 -0600

    modificacion backend.test.js

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -1,6 +1,6 @@
 // tests/backend.test.js
 import request from "supertest";
-import app from "../backend/server.js";
+import app from "../backend/src/app.js";
 
 test("Crear tarea", async () => {
   const res = await request(app).post("/api/tasks").send({ title: "Test" });
